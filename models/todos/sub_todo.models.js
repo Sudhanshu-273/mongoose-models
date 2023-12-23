702813c66e840fc7e4aca32026e41e1aa6894805
Sudhanshu-273 <77341502+Sudhanshu-273@users.noreply.github.com>
2023-12-23 21:52:43 +0530

    Initial commit

---
+++ models/todos/sub_todo.models.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      required: true,
+    },
+    todoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Todo',
+    },
+  },
+  { timestamps: true }
+);
+
+export const SubTodo = mongoose.model('SubTodo', subTodoSchema);
---
+++ models/todos/user.models.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password must not be empty'],
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
